928a59e5bbb67b5a76b54f44f84b0d9365ad4bdd
林懿 <790577263@qq.com>
2024-09-03 18:42:56 +0800

    更新了格式化的插件formatter，格式化命令 <leader>f     ，建议macOS版本也同步一下

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,5 @@
 return {
+	--LSP
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
@@ -19,166 +20,98 @@ return {
 			require("plugins/lspconfig/config")()
 		end,
 	},
+	--Format
 	-- {
-	-- 	"williamboman/mason.nvim",
+	-- 	"jose-elias-alvarez/null-ls.nvim",
+	-- 	dependencies = { "nvim-lua/plenary.nvim" },
 	-- 	config = function()
-	-- 		require("mason").setup({
-	-- 			ui = {
-	-- 				icons = {
-	-- 					package_installed = "✓",
-	-- 					package_pending = "➜",
-	-- 					package_uninstalled = "✗",
-	-- 				},
-	-- 				border = "rounded",
-	-- 			},
-	-- 		})
+	-- 		local null_ls = require("null-ls")
+	-- 		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	--
+	-- 		null_ls.setup({
+	-- 			border = "rounded",
+	-- 			cmd = { "nvim" },
+	-- 			debounce = 250,
+	-- 			debug = false,
+	-- 			default_timeout = 5000,
+	-- 			diagnostic_config = {},
+	-- 			diagnostics_format = "#{m}",
+	-- 			fallback_severity = vim.diagnostic.severity.ERROR,
+	-- 			log_level = "warn",
+	-- 			notify_format = "[null-ls] %s",
+	-- 			on_init = nil,
+	-- 			on_exit = nil,
+	-- 			root_dir = require("null-ls.utils").root_pattern(".null-ls-root", "Makefile", ".git"),
+	-- 			should_attach = nil,
+	-- 			sources = nil,
+	-- 			temp_dir = nil,
+	-- 			update_in_insert = false,
+	-- 			-- formatting on save
+	-- 			--on_attach = function(client, bufnr)
+	-- 			--	if client.supports_method("textDocument/formatting") then
+	-- 			--		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+	-- 			--		vim.api.nvim_create_autocmd("BufWritePre", {
+	-- 			--			group = augroup,
+	-- 			--			buffer = bufnr,
+	-- 			--			callback = function()
+	-- 			--				vim.lsp.buf.format({ bufnr = bufnr })
+	-- 			--			end,
+	-- 			--		})
+	-- 			--	end
+	-- 			--end,
+	-- 		}) -- end of setup
 	-- 	end,
 	-- },
 
 	-- {
-	-- 	"williamboman/mason-lspconfig.nvim",
+	-- 	"MunifTanjim/prettier.nvim",
 	-- 	config = function()
-	-- 		local capabilities = vim.lsp.protocol.make_client_capabilities()
-	-- 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-	--
-	-- 		local servers = {
-	-- 			-- clangd = {},
-	-- 			-- gopls = {},
-	-- 			-- pyright = {},
-	-- 			-- rust_analyzer = {},
-	-- 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-	-- 			--
-	-- 			-- Some languages (like typescript) have entire language plugins that can be useful:
-	-- 			--    https://github.com/pmizio/typescript-tools.nvim
-	-- 			--
-	-- 			-- But for many setups, the LSP (`tsserver`) will work just fine
-	-- 			tsserver = {
-	-- 				"pmizio/typescript-tools.nvim",
-	-- 				dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-	-- 				opts = {},
+	-- 		require("prettier").setup({
+	-- 			bin = "prettier", -- 或者你可以指定 prettier 的绝对路径
+	-- 			filetypes = {
+	-- 				"css",
+	-- 				"graphql",
+	-- 				"html",
+	-- 				"javascript",
+	-- 				"javascriptreact",
+	-- 				"json",
+	-- 				"less",
+	-- 				"markdown",
+	-- 				"scss",
+	-- 				"typescript",
+	-- 				"typescriptreact",
+	-- 				"yaml",
+	-- 				"rust"
 	-- 			},
-	-- 			--
-	--
-	-- 			lua_ls = {
-	-- 				-- cmd = {...},
-	-- 				-- filetypes = { ...},
-	-- 				-- capabilities = {},
-	-- 				settings = {
-	-- 					Lua = {
-	-- 						completion = {
-	-- 							callSnippet = "Replace",
-	-- 						},
-	-- 						-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-	-- 						-- diagnostics = { disable = { 'missing-fields' } },
-	-- 					},
-	-- 				},
-	-- 			},
-	-- 		}
-	-- 		local ensure_installed = vim.tbl_keys(servers or {})
-	-- 		vim.list_extend(ensure_installed, {
-	-- 			"stylua", -- Used to format Lua code
-	-- 		})
-	--
-	-- 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-	-- 		require("mason-lspconfig").setup()
-	-- 		require("mason-lspconfig").setup_handlers({
-	-- 			-- The first entry (without a key) will be the default handler
-	-- 			-- and will be called for each installed server that doesn't have
-	-- 			-- a dedicated handler.
-	-- 			function(server_name) -- default handler (optional)
-	-- 				local server = servers[server_name] or {}
-	-- 				server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-	-- 				require("lspconfig")[server_name].setup({})
-	-- 			end,
-	-- 			-- Next, you can provide a dedicated handler for specific servers.
-	-- 			-- For example, a handler override for the `rust_analyzer`:
-	-- 			["rust_analyzer"] = function()
-	-- 				require("rust-tools").setup({})
-	-- 			end,
+	-- 			-- 可以添加其他 prettier 的配置选项
 	-- 		})
 	-- 	end,
 	-- },
 
 	{
-		"jose-elias-alvarez/null-ls.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		"mhartington/formatter.nvim",
 		config = function()
-			local null_ls = require("null-ls")
-			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-			null_ls.setup({
-				border = "rounded",
-				cmd = { "nvim" },
-				debounce = 250,
-				debug = false,
-				default_timeout = 5000,
-				diagnostic_config = {},
-				diagnostics_format = "#{m}",
-				fallback_severity = vim.diagnostic.severity.ERROR,
-				log_level = "warn",
-				notify_format = "[null-ls] %s",
-				on_init = nil,
-				on_exit = nil,
-				root_dir = require("null-ls.utils").root_pattern(".null-ls-root", "Makefile", ".git"),
-				should_attach = nil,
-				sources = nil,
-				temp_dir = nil,
-				update_in_insert = false,
-				-- formatting on save
-				--on_attach = function(client, bufnr)
-				--	if client.supports_method("textDocument/formatting") then
-				--		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-				--		vim.api.nvim_create_autocmd("BufWritePre", {
-				--			group = augroup,
-				--			buffer = bufnr,
-				--			callback = function()
-				--				vim.lsp.buf.format({ bufnr = bufnr })
-				--			end,
-				--		})
-				--	end
-				--end,
-			}) -- end of setup
-		end,
-	},
-
-	-- {
-	-- 	"jay-babu/mason-null-ls.nvim",
-	-- 	event = { "BufReadPre", "BufNewFile" },
-	-- 	dependencies = {
-	-- 		"williamboman/mason.nvim",
-	-- 		"jose-elias-alvarez/null-ls.nvim",
-	-- 	},
-	-- 	config = function()
-	-- 		require("mason-null-ls").setup({
-	-- 			automatic_installation=true,
-	-- 			automatic_setup = true,
-	-- 			ensure_installed = { "shfmt", "stylua","eslint_d", "prettier" },--[[ ,"eslint_d" ]]
-	-- 			handlers = {},
-	-- 		})
-	-- 	end,
-	-- },
-	{
-		"MunifTanjim/prettier.nvim",
-		config = function()
-			require("prettier").setup({
-				bin = "prettier", -- 或者你可以指定 prettier 的绝对路径
-				filetypes = {
-					"css",
-					"graphql",
-					"html",
-					"javascript",
-					"javascriptreact",
-					"json",
-					"less",
-					"markdown",
-					"scss",
-					"typescript",
-					"typescriptreact",
-					"yaml",
-					"rust"
-				},
-				-- 可以添加其他 prettier 的配置选项
+			local ft_names = {
+				"javascript",
+				"javascriptreact",
+				"typescript",
+				"typescriptreact"
+			}
+			local filetype = {}
+			for _, ft_name in ipairs(ft_names) do
+				filetype[ft_name] = {
+					--上述filetype，都直接对应目录下的prettier
+					require("formatter.filetypes." .. ft_name).prettier
+				}
+			end
+			require('formatter').setup({
+				filetype = filetype
 			})
-		end,
-	},
+			--keymap设置
+			local keymap = vim.keymap.set
+			local opts = { noremap = true }
+			keymap("n", "<leader>f", "<cmd>Format<CR>", opts)
+			keymap("n", "<leader>F", "<cmd>Formatwrite<CR>", opts)
+		end
+	}
 }
