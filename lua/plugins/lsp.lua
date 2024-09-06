fe8841c17cb63f59d427d3daf8627612f7cde603
林懿 <790577263@qq.com>
2024-09-06 14:44:23 +0800

    i need prettier

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,72 +21,72 @@ return {
 		end,
 	},
 	--Format
-	-- {
-	-- 	"jose-elias-alvarez/null-ls.nvim",
-	-- 	dependencies = { "nvim-lua/plenary.nvim" },
-	-- 	config = function()
-	-- 		local null_ls = require("null-ls")
-	-- 		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-	--
-	-- 		null_ls.setup({
-	-- 			border = "rounded",
-	-- 			cmd = { "nvim" },
-	-- 			debounce = 250,
-	-- 			debug = false,
-	-- 			default_timeout = 5000,
-	-- 			diagnostic_config = {},
-	-- 			diagnostics_format = "#{m}",
-	-- 			fallback_severity = vim.diagnostic.severity.ERROR,
-	-- 			log_level = "warn",
-	-- 			notify_format = "[null-ls] %s",
-	-- 			on_init = nil,
-	-- 			on_exit = nil,
-	-- 			root_dir = require("null-ls.utils").root_pattern(".null-ls-root", "Makefile", ".git"),
-	-- 			should_attach = nil,
-	-- 			sources = nil,
-	-- 			temp_dir = nil,
-	-- 			update_in_insert = false,
-	-- 			-- formatting on save
-	-- 			--on_attach = function(client, bufnr)
-	-- 			--	if client.supports_method("textDocument/formatting") then
-	-- 			--		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-	-- 			--		vim.api.nvim_create_autocmd("BufWritePre", {
-	-- 			--			group = augroup,
-	-- 			--			buffer = bufnr,
-	-- 			--			callback = function()
-	-- 			--				vim.lsp.buf.format({ bufnr = bufnr })
-	-- 			--			end,
-	-- 			--		})
-	-- 			--	end
-	-- 			--end,
-	-- 		}) -- end of setup
-	-- 	end,
-	-- },
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local null_ls = require("null-ls")
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-	-- {
-	-- 	"MunifTanjim/prettier.nvim",
-	-- 	config = function()
-	-- 		require("prettier").setup({
-	-- 			bin = "prettier", -- 或者你可以指定 prettier 的绝对路径
-	-- 			filetypes = {
-	-- 				"css",
-	-- 				"graphql",
-	-- 				"html",
-	-- 				"javascript",
-	-- 				"javascriptreact",
-	-- 				"json",
-	-- 				"less",
-	-- 				"markdown",
-	-- 				"scss",
-	-- 				"typescript",
-	-- 				"typescriptreact",
-	-- 				"yaml",
-	-- 				"rust"
-	-- 			},
-	-- 			-- 可以添加其他 prettier 的配置选项
-	-- 		})
-	-- 	end,
-	-- },
+			null_ls.setup({
+				border = "rounded",
+				cmd = { "nvim" },
+				debounce = 250,
+				debug = false,
+				default_timeout = 5000,
+				diagnostic_config = {},
+				diagnostics_format = "#{m}",
+				fallback_severity = vim.diagnostic.severity.ERROR,
+				log_level = "warn",
+				notify_format = "[null-ls] %s",
+				on_init = nil,
+				on_exit = nil,
+				root_dir = require("null-ls.utils").root_pattern(".null-ls-root", "Makefile", ".git"),
+				should_attach = nil,
+				sources = nil,
+				temp_dir = nil,
+				update_in_insert = false,
+				-- formatting on save
+				--on_attach = function(client, bufnr)
+				--	if client.supports_method("textDocument/formatting") then
+				--		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				--		vim.api.nvim_create_autocmd("BufWritePre", {
+				--			group = augroup,
+				--			buffer = bufnr,
+				--			callback = function()
+				--				vim.lsp.buf.format({ bufnr = bufnr })
+				--			end,
+				--		})
+				--	end
+				--end,
+			}) -- end of setup
+		end,
+	},
+
+	{
+		"MunifTanjim/prettier.nvim",
+		config = function()
+			require("prettier").setup({
+				bin = "prettier", -- 或者你可以指定 prettier 的绝对路径
+				filetypes = {
+					"css",
+					"graphql",
+					"html",
+					"javascript",
+					"javascriptreact",
+					"json",
+					"less",
+					"markdown",
+					"scss",
+					"typescript",
+					"typescriptreact",
+					"yaml",
+					"rust"
+				},
+				-- 可以添加其他 prettier 的配置选项
+			})
+		end,
+	},
 
 	{
 		"mhartington/formatter.nvim",
