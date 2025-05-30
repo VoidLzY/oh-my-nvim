e5fbbb03495ab41a623fa4c9b9aba483573f97e3
林懿 <790577263@qq.com>
2025-05-30 16:05:14 +0800

    new x,perfect

--- lua/plugins/lspconfig/config.lua
+++ lua/plugins/lspconfig/config.lua
@@ -8,7 +8,7 @@ return function()
 	vim.keymap.set("n", "<space>=", vim.diagnostic.goto_next)
 	vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
-	vim.fn.sign_define("DiagnosticSignError", { text = "✖", texthl = "DiagnosticSignError" })
+	vim.fn.sign_define("DiagnosticSignError", { text = "❌", texthl = "DiagnosticSignError" })
 	vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
 	vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
 	vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
